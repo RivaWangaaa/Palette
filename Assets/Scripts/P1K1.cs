8961776bf31b8fc34faba97cef3ad9391598e4d5
Yanxi <yw2197@myu.edu>
2021-10-20 23:28:46 -0700

    Added partial NPC movement mechanism;

---
+++ Assets/Scripts/P1K1.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1K1 : MonoBehaviour
+{
+    public GameObject p1;
+    public GameObject k1;
+    public GameObject p1k1;
+
+    private GameObject p1Spawn;
+    public GameObject p1k1Spawn;
+
+    public Transform[] p1Targets;
+    public Transform[] k1Targets;
+    public Transform[] p1k1Targets;
+
+    public float speed;
+
+    private int p1Current;
+    private int k1Current;
+    private int p1k1Current;
+
+    public Vector3 d2StartPos;
+    public Vector3 d2PickupPos;
+
+    private bool p1k1Bool = false;
+    
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate p1
+        p1Spawn = Instantiate(p1,d2StartPos,Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (p1k1Spawn != null)
+        {
+            //p1 move start
+            if (p1Spawn != null && p1Spawn.transform.position != p1Targets[p1Current].position)
+            {
+                Vector3 p1Pos = Vector3.MoveTowards(p1Spawn.transform.position, p1Targets[p1Current].position, speed);
+                p1Spawn.GetComponent<Rigidbody>().MovePosition(p1Pos);
+            }
+            else if (p1Spawn != null && p1Current != p1Targets.Length - 1)
+            {
+                p1Current = (p1Current + 1) % p1Targets.Length;
+                Debug.Log("D2 Pickup POS: " + p1Spawn.transform.position);
+            }
+            //p1 move ends
+            else if (p1Current == p1Targets.Length - 1)
+            {
+                Debug.Log("p1Current: " + p1Current);
+                //k1 move start when p1 move ends
+                if (k1 != null && k1.transform.position != k1Targets[k1Current].position)
+                {
+                    Vector3 k1Pos = Vector3.MoveTowards(k1.transform.position, k1Targets[k1Current].position, speed);
+                    k1.GetComponent<Rigidbody>().MovePosition(k1Pos);
+                }
+                else if (k1 != null && k1Current != k1Targets.Length - 1)
+                {
+                    k1Current = (k1Current + 1) % k1Targets.Length;
+                }
+                //k1 move ends
+                else if (k1Current == k1Targets.Length - 1)
+                {
+                    //Destroy p1 and k1
+                    Destroy(k1);
+                    Destroy(p1Spawn);
+
+                    //At the same pos spawn p1k1
+                    if (p1k1Bool == false)
+                    {
+                        p1k1Spawn = Instantiate(p1k1, d2PickupPos, Quaternion.identity);
+                        p1k1Bool = true;
+                    }
+
+                    //p1k1 move starts
+                    if (p1k1Spawn.transform.position != p1k1Targets[p1k1Current].position)
+                    {
+                        Vector3 p1k1Pos = Vector3.MoveTowards(p1k1Spawn.transform.position,
+                            p1k1Targets[p1k1Current].position, speed);
+                        p1k1Spawn.GetComponent<Rigidbody>().MovePosition(p1k1Pos);
+                    }
+                    else if (p1k1Current != p1k1Targets.Length - 1)
+                    {
+                        p1k1Current = (p1k1Current + 1) % p1k1Targets.Length;
+                    }
+                    else if (p1k1Current == p1k1Targets.Length - 1)
+                    {
+                        Destroy(p1k1Spawn);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/P2K2.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2K2 : MonoBehaviour
+{
+   
+    public GameObject p;
+    public GameObject k;
+    public GameObject pk;
+
+    private GameObject pSpawn;
+    public GameObject pkSpawn;
+
+    public Transform[] pTargets;
+    public Transform[] kTargets;
+    public Transform[] pkTargets;
+
+    public float speed;
+
+    private int pCurrent;
+    private int kCurrent;
+    private int pkCurrent;
+
+    public Vector3 startPos;
+    public Vector3 pickupPos;
+
+    private bool pkBool = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate p1
+        pSpawn = Instantiate(p,startPos,Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (pkSpawn != null)
+        {
+            //p1 move start
+            if (pSpawn != null && pSpawn.transform.position != pTargets[pCurrent].position)
+            {
+                Vector3 pPos = Vector3.MoveTowards(pSpawn.transform.position, pTargets[pCurrent].position, speed);
+                pSpawn.GetComponent<Rigidbody>().MovePosition(pPos);
+            }
+            else if (pSpawn != null && pCurrent != pTargets.Length - 1)
+            {
+                pCurrent = (pCurrent + 1) % pTargets.Length;
+                Debug.Log("D1 Pickup POS: " + pSpawn.transform.position);
+            }
+            //p1 move ends
+            else if (pCurrent == pTargets.Length - 1)
+            {
+                //Debug.Log("p1Current: " + pCurrent);
+                //k1 move start when p1 move ends
+                if (k != null && k.transform.position != kTargets[kCurrent].position)
+                {
+                    Vector3 kPos = Vector3.MoveTowards(k.transform.position, kTargets[kCurrent].position, speed);
+                    k.GetComponent<Rigidbody>().MovePosition(kPos);
+                }
+                else if (k != null && kCurrent != kTargets.Length - 1)
+                {
+                    kCurrent = (kCurrent + 1) % kTargets.Length;
+                }
+                //k1 move ends
+                else if (kCurrent == kTargets.Length - 1)
+                {
+                    //Destroy p1 and k1
+                    Destroy(k);
+                    Destroy(pSpawn);
+
+                    //At the same pos spawn p1k1
+                    if (pkBool == false)
+                    {
+                        pkSpawn = Instantiate(pk, pickupPos, Quaternion.identity);
+                        pkBool = true;
+                    }
+
+                    //p1k1 move starts
+                    if (pkSpawn.transform.position != pkTargets[pkCurrent].position)
+                    {
+                        Vector3 pkPos = Vector3.MoveTowards(pkSpawn.transform.position,
+                            pkTargets[pkCurrent].position, speed);
+                        pkSpawn.GetComponent<Rigidbody>().MovePosition(pkPos);
+                    }
+                    else if (pkCurrent != pkTargets.Length - 1)
+                    {
+                        pkCurrent = (pkCurrent + 1) % pkTargets.Length;
+                    }
+                    else if (pkCurrent == pkTargets.Length - 1)
+                    {
+                        Destroy(pkSpawn);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
