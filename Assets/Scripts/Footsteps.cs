6a44a490d64503e750fd92da75c7d692ef2e3ba7
Walid <ar6749@nyu.edu>
2021-10-21 17:51:48 -0400

    Added footstep and music in the background

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    // Use this for initialization
+    CharacterController cc;
+    void Start () {
+        cc = GetComponent<CharacterController>();
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
