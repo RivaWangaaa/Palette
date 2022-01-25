7e6bd7e65847db4d1e768e4773ab1e7478a92ea3
Nateliu <sl8292@nyu.edu>
2022-01-25 23:48:10 +0800

    add some UI before merge

---
+++ Assets/Scripts/UI/HintsGroupBG.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HintsGroupBG : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float colorAlphaSpeed;
+    private Image pinkImage;
+
+    private void Start()
+    {
+        pinkImage = GetComponent<Image>();
+    }
+
+    private void FixedUpdate()
+    {
+        Color tempColor = pinkImage.color;
+
+        tempColor.a += colorAlphaSpeed * Time.deltaTime;
+
+        pinkImage.color = tempColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        colorAlphaSpeed = 5;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        colorAlphaSpeed = -5;
+    }
+}
