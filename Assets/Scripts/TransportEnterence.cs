b9bae5dd04d3c4cc53434e632d985dc8fbd8c868
Nateliu <sl8292@nyu.edu>
2022-02-11 04:57:15 +0800

    Teleportdoor

---
+++ Assets/Scripts/TransportEnterence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class TransportEnterence : MonoBehaviour
+{
+    public Flowchart tryToEnterFlowchat;
+    public Transform enterClassroomPoint;
+    
+    public void OnInteract()
+    {
+        NPCManager.instance.isHavingConversation = true;
+
+        //lock player's camera when having a conversation
+        GameManager.instance.currentControllingPlayer.GetComponent<MouseLook>().sensitivityX = 0;
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.GetComponent<MouseLook>().sensitivityY = 0;
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.GetComponent<LockMouse>().LockCursor(false);
+    }
+    
+    public void EndInteract()
+    {
+        NPCManager.instance.isHavingConversation = false;
+        GameManager.instance.currentControllingPlayer.GetComponent<MouseLook>().sensitivityX = 8;
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.GetComponent<MouseLook>().sensitivityY = 8;
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.GetComponent<LockMouse>().LockCursor(true);
+    }
+    
+    public void OnEnter()
+    {
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = false;
+        GameManager.instance.currentControllingPlayer.transform.position = enterClassroomPoint.position;
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = true;
+        ExitClassroomController.instance.isPlayerOutsideTheClassroom = false;
+
+    }
+}
