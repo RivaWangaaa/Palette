559375ad8f8c4ebeb2fe95559e0d51b8301f8e21
Nateliu <sl8292@nyu.edu>
2022-03-10 04:04:40 +0800

    Loop1 reset set up

---
+++ Assets/Scripts/StagePlay/Loop0EndGhostTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop0EndGhostTransition : MonoBehaviour
+{
+    public GameObject GhostTransitionDialog;
+    public GameObject mainCameraCanvas;
+    public GameObject ghostTransitionCamera;
+    
+    public void Loop0GhostTransitionSetUp()
+    {
+        GhostTransitionDialog.SetActive(true);
+        mainCameraCanvas.SetActive(true);
+        ghostTransitionCamera.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/StagePlay/Loop0SleepPerformanceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop0SleepPerformanceController : MonoBehaviour
+{
+    public GameObject FallAsleepCam;
+    public GameObject JumpScareCutScene;
+    public GameObject levelDoor;
+    
+    public void Loop0EndBackToSeat()
+    {
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = false;
+        GameManager.instance.currentControllingPlayer.transform.position 
+            = GameManager.instance.currentControllingPlayer.GetComponent<Player>().chairPosition.position;
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = true;
+        FallAsleepCam.SetActive(true);
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    
+    public void Loop0EndWakeUp()
+    {
+        FallAsleepCam.SetActive(false);
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.SetActive(true);
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void Loop0EndJumpScare()
+    {
+        //GameManager.instance.EnterConversationMode();
+        levelDoor.SetActive(false);
+        JumpScareCutScene.SetActive(true);
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        GameManager.instance.currentControllingPlayer.transform.GetChild(0).gameObject.SetActive(false);
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = false;
+        GameManager.instance.currentControllingPlayer.transform.position 
+            = GameManager.instance.currentControllingPlayer.GetComponent<Player>().chairPosition.position;
+        GameManager.instance.currentControllingPlayer.GetComponent<CharacterController>().enabled = true;
+    }
+
+    public void Loop0EedJumpScareSetUp()
+    {
+        
+    }
+}
